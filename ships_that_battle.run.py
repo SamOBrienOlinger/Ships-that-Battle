c01155c6414c59d1dc7276835d9009730781f86b
SamOBrienOlinger <samtobo@yahoo.co.uk>
2022-06-27 09:19:02 +0000

    update text for Player regarding instructions

--- ships_that_battle.run.py
+++ ships_that_battle.run.py
@@ -21,46 +21,65 @@ def create_board(board):
     print('  ---------------')
     row_number = 1
     for row in board:
-        print("%d|%s|" % (row_number, "|" .join(row)))
-    row_number += 1
+        print("%d|%s|" % (row_number, "|".join(row)))
+        row_number += 1
 
 
+# Place 5 random ships on SECRET_BOARD
 def random_ship_location(board):
     for ship in range(5):
         ship_row, ship_column = randint(0, 7), randint(0, 7)
-        while [ship_row][ship_column] == 'X':
-            ship_row, ship_column = randint(0, 7), randint(0, 7)
+        while board[ship_row][ship_column] == 'X':
+            ship_row, ship_column = guess_ship_location()
     board[ship_row][ship_column] = 'X'
 
 
 def guess_ship_location():
     row = input('Where will you fire your cannons?!? 1 - 8: ')
     while row not in '12345678':
-        print("Your shots must fired on the map, between 1 - 8")
-        row = input('Where will you fire your cannons?!? 1 - 8: ')
-
+        print("Your shots must be fired on the map, between rows 1 - 8")
+        row = input('Where will you fire your cannons?!? row 1 - 8: ')
     column = input('Where will you fire your cannons?!? A - H: ').upper()
     while column not in 'ABCDEFGH':
         print("Your shots must fired on the map, between A - H")
         column = input('Where will you fire your cannons?!? A - H: ').upper()
-
     return int(row) - 1, letters_to_numbers[column]
 
-# Need a way to stop User from not inputting anything
+# Need a way to stop User from not inputting nothing
 
 
-def ship_hits(board):
+def all_ships_hit(board):
     count = 0
-    for rows in board:
+    for row in board:
         for column in row:
             if column == 'X':
                 count += 1
     return count
 
-random_ship_location(SECRET_BOARD)
 
-turns = 10
-# while turns > 0
+if __name__ == "__main__":
+    random_ship_location(SECRET_BOARD)
+    turns = 10
+    while turns > 0:
+        print("Fire at the Enemy to Battle their Ships")
+        print(PLAYER_BOARD)
+        row, column = guess_ship_location()
+        if SECRET_BOARD[row][column] == '-':
+            print("Deja Vu, try a different location")
+        elif SECRET_BOARD[row][column] == "X":
+            print("Huzzah, a hit!")
+            PLAYER_BOARD[row][column] = "X"
+            turns -= 1
+        else:
+            print("A miss, and a splash. Better luck next turn!")
+            SECRET_BOARD[row][column] = "-"
+            turns -= 1
+        if all_ships_hit == 5:
+            print("Congratulations Captain, you rule the waves!")
+            break
+        print("Captain, you have " + str(turns) + " cannon balls left")
+        if turns == 0:
+            print("You ran out of cannonballs")
 
 create_board(SECRET_BOARD)
 create_board(PLAYER_BOARD)
